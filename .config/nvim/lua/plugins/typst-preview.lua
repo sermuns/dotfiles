fbe691c5afdebfbe739f95fe232ff777b9011c4f
Samuel Åkesson <samak519@student.liu.se>
2026-01-05 11:55:16 +0100

    no port typst-preview

--- .config/nvim/lua/plugins/typst-preview.lua
+++ .config/nvim/lua/plugins/typst-preview.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     -- invert_colors = 'auto',
     open_cmd = 'xdg-open %s &>/dev/null',
-    port = 23626,
+    -- port = 23626,
     dependencies_bin = {
       ['tinymist'] = '/home/sermuns/.local/share/nvim/mason/bin/tinymist',
     },
