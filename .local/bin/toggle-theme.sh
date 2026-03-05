b2449687d66cb8a36cf13cc39a7a28039d65d463
Samuel Åkesson <samak519@student.liu.se>
2026-03-05 18:35:57 +0100

    adwaita too

--- .local/bin/toggle-theme.sh
+++ .local/bin/toggle-theme.sh
@@ -7,10 +7,12 @@ if [ -f "$MODE_FILE" ] && grep -q "dark" "$MODE_FILE"; then
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
     sed -i 's/^ColorScheme=.*$/ColorScheme=Breeze/' ~/.config/kdeglobals 2>/dev/null
+    GTK_THEME=Adwaita
     echo "light" > "$MODE_FILE"
 else
     gsettings set org.gnome.desktop.interface color-scheme 'default'
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita:dark'
     sed -i 's/^ColorScheme=.*$/ColorScheme=BreezeDark/' ~/.config/kdeglobals 2>/dev/null
+    GTK_THEME=Adwaita:dark
     echo "dark" > "$MODE_FILE"
 fi
