6a6ac712e0a056c2250458f199ccbc7521171cfe
Samuel Åkesson <samak519@student.liu.se>
2026-04-11 20:36:46 +0200

    pin to version

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -1,6 +1,6 @@
 return {
   'mrcjkb/rustaceanvim',
-  -- version = '^7', -- Recommended
+  version = '^9',
   lazy = false, -- This plugin is already lazy
   config = function()
     vim.keymap.set('n', 'gD', function()
