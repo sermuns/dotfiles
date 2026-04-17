130cca439a2dd3c52755a7984097aadb89d6ed13
Samuel Åkesson <samak519@student.liu.se>
2026-04-17 11:29:39 +0200

    use normal lsp hover

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -11,14 +11,13 @@ return {
       vim.cmd.RustLsp 'codeAction' -- supports rust-analyzer's grouping
       -- or vim.lsp.buf.codeAction() if you don't want grouping.
     end, { silent = true, buffer = bufnr })
-    vim.keymap.set(
-      'n',
-      'K', -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
-      function()
-        vim.cmd.RustLsp { 'hover', 'actions' }
-      end,
-      { silent = true, buffer = bufnr }
-    )
-    --  )
+    -- vim.keymap.set(
+    --   'n',
+    --   'K', -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+    --   function()
+    --     vim.cmd.RustLsp { 'hover', 'actions' }
+    --   end,
+    --   { silent = true, buffer = bufnr }
+    -- )
   end,
 }
