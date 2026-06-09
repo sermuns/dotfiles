ad70ec123353f35417e324e66298f4dcd8dca783
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-09 19:49:36 +0200

    add back godot shiiz

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -79,24 +79,6 @@ require('lazy').setup({
   },
 })
 
--- -- paths to check for project.godot file
--- local paths_to_check = { '/', '/../' }
--- local is_godot_project = false
--- local godot_project_path = ''
--- local cwd = vim.fn.getcwd()
---
--- -- iterate over paths and check
--- for key, value in pairs(paths_to_check) do
---   if vim.uv.fs_stat(cwd .. value .. 'project.godot') then
---     is_godot_project = true
---     godot_project_path = cwd .. value
---     break
---   end
--- end
---
--- -- check if server is already running in godot project path
--- local is_server_running = vim.uv.fs_stat(godot_project_path .. '/server.pipe')
--- -- start server, if not already running
--- if is_godot_project and not is_server_running then
---   vim.fn.serverstart(godot_project_path .. '/server.pipe')
--- end
+if vim.uv.fs_stat(vim.fs.joinpath(cwd, 'project.godot')) and not vim.uv.fs_stat '/tmp/godot.pipe' then
+  vim.fn.serverstart '/tmp/godot.pipe'
+end
