abf13c508b1fa568a61b7c6b584e545a9cd30745
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-28 18:52:34 +0200

    add lua_ls and a lotta shit and format

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,8 @@ vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.list = true
 
+vim.opt.completeopt = { "menu", "menuone", "noinsert" }
+
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
 vim.keymap.set({ 't', 'i' }, '<C-h>', '<C-\\><C-n><C-w>h')
@@ -32,6 +34,7 @@ vim.keymap.set('n', '<F3>', vim.cmd.source)
 vim.keymap.set('n', '<F2>', vim.lsp.buf.rename)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references)
 vim.keymap.set('n', 'gca', vim.lsp.buf.code_action)
+vim.keymap.set('n', '<S-M-f>', vim.lsp.buf.format)
 
 vim.keymap.set('v', '<C-c>', 'gc', { remap = true })
 vim.keymap.set('n', '<C-c>', 'gcc', { remap = true })
@@ -39,42 +42,50 @@ vim.keymap.set('n', '<C-c>', 'gcc', { remap = true })
 vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
 vim.keymap.set({ 'n', 'v' }, '<leader>p', [["+p]])
 
+vim.keymap.set('n', '<Esc>', vim.cmd.nohlsearch)
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  callback = function()
-    vim.hl.on_yank()
-  end,
+	desc = 'Highlight when yanking (copying) text',
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
-vim.api.nvim_create_user_command('GitBlameLine', function()
-  local line_number = vim.fn.line('.')
-  local filename = vim.api.nvim_buf_get_name(0)
-  print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
-end, { desc = 'Print the git blame for the current line' })
 
 
 vim.cmd('packadd! nohlsearch')
 
 vim.pack.add({
-  -- Quickstart configs for LSP
-  'https://github.com/neovim/nvim-lspconfig',
-  -- Fuzzy picker
-  'https://github.com/ibhagwan/fzf-lua',
-  -- Autocompletion
-  'https://github.com/nvim-mini/mini.completion',
-  -- Enhanced quickfix/loclist
-  'https://github.com/stevearc/quicker.nvim',
-  -- Git integration
-  'https://github.com/lewis6991/gitsigns.nvim',
-{
-  src = 'https://github.com/mrcjkb/rustaceanvim',
-  version = vim.version.range('^9')
-}
+	-- Quickstart configs for LSP
+	'https://github.com/neovim/nvim-lspconfig',
+	-- Fuzzy picker
+	'https://github.com/ibhagwan/fzf-lua',
+	-- Autocompletion
+	{ src = 'https://github.com/nvim-mini/mini.completion', version = 'stable' },
+	-- Enhanced quickfix/loclist
+	'https://github.com/stevearc/quicker.nvim',
+	-- Git integration
+	'https://github.com/lewis6991/gitsigns.nvim',
+	{
+		src = 'https://github.com/mrcjkb/rustaceanvim',
+		version = vim.version.range('^9')
+	},
+	{
+		src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
+		version = vim.version.range('3')
+	},
+	-- dependencies
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/MunifTanjim/nui.nvim",
+	-- optional, but recommended
+	"https://github.com/nvim-tree/nvim-web-devicons",
 })
+
+vim.lsp.enable 'lua_ls'
 
 require('fzf-lua').setup { fzf_colors = true }
 require('mini.completion').setup {}
 require('quicker').setup {}
 require('gitsigns').setup {}
 
+vim.keymap.set('n', '\\', '<cmd>Neotree toggle<cr>')
