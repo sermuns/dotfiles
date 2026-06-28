97600005eaef16a4e28e4185c7dffaa924e2f845
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-28 18:21:07 +0200

    cooking

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,27 +1,43 @@
 vim.g.mapleader = ' '
 
-vim.o.number = false
+vim.o.number = true
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
-vim.o.cursorline = true -- Highlight the line where the cursor is on.
-vim.o.scrolloff = 10 -- Keep this many screen lines above/below the cursor.
-vim.o.list = true -- Show <tab> and trailing spaces.
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.list = true
 
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
--- Map <A-j>, <A-k>, <A-h>, <A-l> to navigate between windows in any modes
-vim.keymap.set({ 't', 'i' }, '<A-h>', '<C-\\><C-n><C-w>h')
-vim.keymap.set({ 't', 'i' }, '<A-j>', '<C-\\><C-n><C-w>j')
-vim.keymap.set({ 't', 'i' }, '<A-k>', '<C-\\><C-n><C-w>k')
-vim.keymap.set({ 't', 'i' }, '<A-l>', '<C-\\><C-n><C-w>l')
-vim.keymap.set({ 'n' }, '<A-h>', '<C-w>h')
-vim.keymap.set({ 'n' }, '<A-j>', '<C-w>j')
-vim.keymap.set({ 'n' }, '<A-k>', '<C-w>k')
-vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
+vim.keymap.set({ 't', 'i' }, '<C-h>', '<C-\\><C-n><C-w>h')
+vim.keymap.set({ 't', 'i' }, '<C-j>', '<C-\\><C-n><C-w>j')
+vim.keymap.set({ 't', 'i' }, '<C-k>', '<C-\\><C-n><C-w>k')
+vim.keymap.set({ 't', 'i' }, '<C-l>', '<C-\\><C-n><C-w>l')
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
 
-vim.keymap.set({ 'n' }, '<F2>', vim.cmd.source )
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set({ 'n', 'v' }, '<leader>p', [["+p]])
+
+vim.keymap.set('n', '<A-h>', vim.cmd.bprevious)
+vim.keymap.set('n', '<A-l>', vim.cmd.bnext)
+vim.keymap.set('n', '<A-q>', vim.cmd.close)
+
+vim.keymap.set('n', '<F3>', vim.cmd.source)
+
+vim.keymap.set('n', '<F2>', vim.lsp.buf.rename)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+vim.keymap.set('n', 'gca', vim.lsp.buf.code_action)
+
+vim.keymap.set('v', '<C-c>', 'gc', { remap = true })
+vim.keymap.set('n', '<C-c>', 'gcc', { remap = true })
+
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set({ 'n', 'v' }, '<leader>p', [["+p]])
 
 
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -32,7 +48,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.api.nvim_create_user_command('GitBlameLine', function()
-  local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
+  local line_number = vim.fn.line('.')
   local filename = vim.api.nvim_buf_get_name(0)
   print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
 end, { desc = 'Print the git blame for the current line' })
@@ -51,9 +67,14 @@ vim.pack.add({
   'https://github.com/stevearc/quicker.nvim',
   -- Git integration
   'https://github.com/lewis6991/gitsigns.nvim',
+{
+  src = 'https://github.com/mrcjkb/rustaceanvim',
+  version = vim.version.range('^9')
+}
 })
 
 require('fzf-lua').setup { fzf_colors = true }
 require('mini.completion').setup {}
 require('quicker').setup {}
 require('gitsigns').setup {}
+
