e23d6149e4e6b288d26b9a6fee7de57ce15336e5
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-29 02:28:12 +0200

    move rust shiz

---
+++ .config/nvim/ftplugin/rust.lua
@@ -0,0 +1,23 @@
+local tcp = vim.uv.new_tcp()
+tcp:connect("127.0.0.1", 27631, function(err)
+	tcp:close()
+
+	if err then
+		-- lspmux is not responding
+		return
+	end
+
+	vim.lsp.config('rust_analyzer', {
+		cmd = vim.lsp.rpc.connect("127.0.0.1", 27631),
+		settings = {
+			["rust-analyzer"] = {
+				lspMux = {
+					version = "1",
+					method = "connect",
+					server = "rust-analyzer",
+				},
+			},
+		}
+	})
+end
+)
