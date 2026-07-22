2d6f56ae6b66c99eb7a11c0da209a579c80517a1
Samuel Åkesson <sermuns@lysator.liu.se>
2026-07-22 16:37:50 +0200

    fix wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,10 +2,20 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.font = wezterm.font 'Monaspace Krypton'
-config.font_size = 30
+config.font_size = 40
+
+local padding = 0
+config.window_padding = {
+	left = padding,
+	right = padding,
+	top = padding,
+	bottom = padding,
+}
 
 config.color_scheme = 'tokyonight_night'
 
 config.enable_tab_bar = false
+
+config.default_prog = { 'bash', '-i' }
 
 return config
