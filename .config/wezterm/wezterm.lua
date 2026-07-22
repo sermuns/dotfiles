b897a32dc9cc0f8be60527410d8492835dd3ad12
Samuel Åkesson <sermuns@lysator.liu.se>
2026-07-22 14:20:51 +0200

    add wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'Monaspace Krypton'
+config.font_size = 30
+
+config.color_scheme = 'tokyonight_night'
+
+config.enable_tab_bar = false
+
+return config
